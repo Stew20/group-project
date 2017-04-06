845cd33da14628e69323cb05e56bedfa4f229b66
Greg Griffith <cryptounitedteam@gmail.com>
2017-04-06 13:51:46 -0400

    side nav file template
    side nav file template

---
+++ sidenav.php
@@ -0,0 +1,34 @@
+<?php
+
+//page reference for info pull from nav button click 
+
+/* Home */
+
+// intentionally left blank
+
+
+
+/* tutorials */
+
+//tutorial list
+
+
+
+
+/* commands */
+
+//command list
+
+
+
+/* quizzes */
+
+//quiz stuff
+
+
+
+/* reources */
+
+// resource stuff
+
+?>
