4066b8d24d0331c14af455b0aa223bcd09d8d904
Cenric <eric.cen@hotmail.com>
2017-04-10 16:17:43 -0400

    Footer and Resources .php Files
    Created template for Footer and Resources files

--- footer.php
+++ footer.php
@@ -1,2 +1,6 @@
 <footer>		
+    <p> © 2017 Stew20 </p>
+    <p> Contact Info: <br>
+    xxx1234@rit.edu <br>
+    (123)-456-7890</p>
 </footer>
--- resources.php
+++ resources.php
@@ -5,10 +5,11 @@
 ?>
 <body>
 
+<p> Below is a list of all the resources that were used or referenced to while creating this website. Further down we have also found a number of other places that seemed interesting or extremely relevant to Unix and have listed them in case you wanted to visit other places for more information.   </p>
 
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
-
+<ul>
+</ul>
+    
 <?php
 	include "footer.php";
 ?>
