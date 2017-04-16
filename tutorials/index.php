f18ec2877048f4b913196aa6c757b32dc2674141
Greg Griffith <tfa.productions.1337@gmail.com>
2017-04-16 05:42:48 -0400

    added subsection folders
    added subsection folders for all of the categories.

---
+++ tutorials/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
+</head>
+	<?php
+		include'../header.php';
+	?>
+	<body>
+
+
+		<h1>This is a Heading</h1>
+		<p>This is a paragraph.</p>
+
+	
+
+
+	<?php
+		include'../footer.php';
+	?>
+	</body>
+</html>
