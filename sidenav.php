a9e9d05412f0b158a04e10aca6be2c81cde814c8
Greg Griffith <tfa.productions.1337@gmail.com>
2017-04-16 05:23:59 -0400

    added php for sidenav
    this should be enough to dynamically assign any sidenav list buttons.
    needs to be styled with css at some point

--- sidenav.php
+++ sidenav.php
@@ -1,34 +1,38 @@
 <?php
+//pull session variable
+$section = $_SESSION["sidenav"];
 
-//page reference for info pull from nav button click 
+// use session variable sidenav to do stuff
 
+if($section = "home")
+{
 /* Home */
-
-// intentionally left blank
-
-
-
-/* tutorials */
-
-//tutorial list
-
-
-
-
-/* commands */
-
-//command list
-
-
-
-/* quizzes */
-
-//quiz stuff
-
-
-
-/* reources */
-
-// resource stuff
-
+//intentionally left blank
+}
+else
+{
+	echo "<ul>";
+	/* tutorials, commands, quizzes, reources */
+	$section_table = "sidenav_".$section;
+	$sql = "select nav_index,selected from '". $section_table ."'";
+	$result = $conn->query($sql);
+	if($result->num_rows > 0){
+		while($row=$result->FETCH_ASSOC()){
+			if($row['selected'] == 1){
+				echo '<li class="selected_sidenav">';
+				echo $row['nav_index'];
+				// need to add something here in the li for an href so that it auto links to the page it needs to go to, probably additional field in DB
+			}
+			else{
+				echo "<li>"
+				echo $row['nav_index'];
+				// need to add something here in the li for an href so that it auto links to the page it needs to go to, probably additional field in DB
+			}
+			echo "</li>";
+		}
+	}
+	else{
+		echo "0 results, you did something bad";
+	}
+}
 ?>
