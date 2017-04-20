2fb38a75e8f165289b8144f60ce0524dd455200e
Greg Griffith <cryptounitedteam@gmail.com>
2017-04-20 13:26:51 -0400

    fixed this php
    fixed this php

--- process_nav_button.php
+++ process_nav_button.php
@@ -1,23 +1,36 @@
 <?php
-$destination = $_POST['pageTo'];
-$_SESSION["sidenav"] = $destination;
+	$destination = $_POST['pageTo'];
+	$_SESSION["sidenav"] = $destination;
 
-include "php_connection.php";
+	include "php_connection.php";
 
-$sql = "select id from sections where selected=1";
-$result = $conn->query($sql);
-if($result->num_rows > 0){
+	$sql = "select id from sections where selected=1";
+	$result = $conn->query($sql);
+	if($result->num_rows > 0){
 	// this loop should reselt all selected to 0 so that we can turn the proper one to =1
-	while($row=$result->FETCH_ASSOC()){
-		$sql2 = "update sections set selected=0 where id=". $row['id'] . "";
-		$result = $conn->query($sql2);
+		while($row=$result->FETCH_ASSOC()){
+			$sql2 = "update sections set selected=0 where id=". $row['id'] . "";
+			$result2 = $conn->query($sql2);
+			if($result2->num_rows > 0){
+				$sql3 = "update sections set selected=1 where section_name='" . $destination . "'";
+				$result = $conn->query($sql3);
+				if($result3->num_rows > 0){
+					// intentionally left blank
+				}
+				else{
+					echo("result3 failure");
+					die();
+				}
+			}
+			else{
+				echo("result2 failure");
+				die();
+			}
+		}
 	}
-}
-$sql3 = "update sections set selected=1 where section_name='" . $destination . "'";
-$result = $conn->query($sql3);
-
-$domain = "http://kelvin.ist.rit.edu/";
-$dest_string = $domain . "~stew20/group-project/". $destination ."/index.php";
-header($dest_string);
-die();
+	else
+	{
+		echo("result failure");
+		die();
+	}
 ?>
