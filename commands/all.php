2ecffbd17d8a9fccf053ba7602c64ef45d534e13
yalingL <yxl4427@g.rit.edu>
2017-04-24 22:30:13 -0400

    change css style

--- commands/all.php
+++ commands/all.php
@@ -1,140 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-
-	<head>
-	<title>Command</title>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
-	<style>
-	table {
-    border-collapse: collapse;
-    width: 100%;
-	}
-
-	th, td {
-    text-align: left;
-    padding: 8px;
-	}
-
-	tr:nth-child(even){background-color: #e6e6e6}
+<?php
+	$path = "../";
+	include $path."header.php";
+?>
 	
-	#nav ul {
-			list-style-type: none;
-			margin: 0;
-			padding: 0;
-			overflow: hidden;
-			background-color: #6C648B;
-		}
-
-		#nav li {
-			float: left;
-		}
-
-		#nav li ul, li li ul {
-			display: none;
-			position: absolute;
-			font-weight: normal;
-		}
-		
-		#nav li li {
-			display: block;
-			float: none;
-			background-color: #6C648B;
-			border: 0;
-		}
-
-		#nav li a {
-			display: block;
-			color: #EDFFCA;
-			text-align: center;
-			padding: 14px 16px;
-			text-decoration: none;
-		}
-		
-		#nav li a:hover:not(.active) {
-			background-color: #EDFFCA;
-			color: #6C648B;
-		}
-
-		#nav li:hover ul{
-			display: block;
-		}
-		
-		#nav li ul a{
-			float: left;
-		
-		}
-		
-		.home{
-			padding: 0 0;
-			width: 46px;
-			height: 46px;
-			bottom: 50px;
-		}
-		
-		.active {
-			background-color: #EDFFCA;
-			color: #6C648B;
-		}
-		/**
-			need an @ media Query at 471px, or 540 if we add a search icon 
-		**/
-	/*
-	body  {
-    background-image: url("assets/images/10Unix05.jpg");
-    width:100vw;
-    height:500px;
-    background-repeat:no-repeat;
-    }
-	*/
-	</style>	
-	</head>
 	
 	<body>
 	<div class="tran">
- 	   <img src="assets/images/code3.jpg" alt="picture" style="width:100vw;height:400px;">
+ 	   <?php echo('<img src="'.$path.'assets/images/code3.jpg" alt="picture" style="width:100vw;height:400px;">'); ?>
 	</div>
 	
-	
-	<div id="nav">
-            <ul>
-                <!-- <li><a class="active" href="#home">Home</a></li> -->
-                <!-- <li><a class="home" href="#home"><img src = "HomeButton2.png" alt = "Home Button" style = "width: 35px; height: 35px;"> </li></a> -->
-                <li><a href="#home">Home</a></li>
-                <li><a href="#tutorials">Tutorials</a>
-                    <ul>
-                        <li><a href="#tutorial1">1. OS Navigation</a></li>
-                        <li><a href="#tutorial2">2. File Manipulation</a></li>
-                        <li><a href="#tutorial3">3. Filer Permissions</a></li>
-                        <li><a href="#tutorial4">4. Overview</a></li>
-                        <li><a href="#tutorial5">More Tutorials</a></li>	
-                    </ul>
-                </li>
-                <li><a href="#commands">Commands</a>
-                    <ul>
-                        <li><a href="#command1">All Commands</a></li>
-                        <li><a href="#command2">Common Commands</a></li>
-                        <li><a href="#command3">Navigation Commands</a></li>	
-                    </ul>
-                </li>
-                <li><a href="#quizzes">Quizzes</a>
-                    <ul>
-                        <li><a href="#quiz1">Q1. Navigation</a></li>
-                        <li><a href="#quiz2">Q2. Files</a></li>
-                        <li><a href="#quiz3">Q3. Overview</a></li>	
-                    </ul>
-                </li>
-                <li><a href="resources.php">Resources</a>
-                    <ul>
-                        <li><a href="#Recource1">Books</a></li>
-                        <li><a href="#Recource2">PDF's</a></li>
-                        <li><a href="#Recource3">Links</a></li>
-                        <li><a href="#Recource4">Contact Us</a></li>	
-                    </ul>		
-                </li>
-                <!-- <li> <a class="search"> </li> -->
-            </ul>
-        </div> <!-- nav div ends here -->
+	<?php
+		include $path.'nav.php';
+	?>
 
 	<h1>Unix Commands: An Introduction to Common Commands</h1>
 	<div class="border1">
@@ -198,5 +77,8 @@
     </table>
     </div>
     </div>
+	<?php
+		include $path."footer.php";
+	?>		
 	</body>
 </html>
