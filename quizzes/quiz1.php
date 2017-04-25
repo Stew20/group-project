a8cbf4cb35ba10139b9107ab362f9c7bddc008ed
Greg-Griffith <cryptounitedteam@gmail.com>
2017-04-25 11:57:59 -0400

    quiz template
    quiz template. should be functional

---
+++ quizzes/quiz1.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
+</head>
+	<?php
+		include'../header.php';
+	?>
+	<body>
+		<form onSubmit="checkAnswers()">
+			<h1>Quiz1</h1>
+			<div>
+				<h2>Question 1</h2>
+				<input type="radio" name="1" value="A"></input>
+				<input type="radio" name="1" value="B"></input>
+				<input type="radio" name="1" value="C"></input>
+				<input type="radio" name="1" value="D"></input>
+			</div>
+			<div>
+				<h2>Question 2</h2>
+				<input type="radio" name="2" value="A"></input>
+				<input type="radio" name="2" value="B"></input>
+				<input type="radio" name="2" value="C"></input>
+				<input type="radio" name="2" value="D"></input>
+			</div>
+			<div>
+				<h2>Question 3</h2>
+				<input type="radio" name="3" value="A"></input>
+				<input type="radio" name="3" value="B"></input>
+				<input type="radio" name="3" value="C"></input>
+				<input type="radio" name="3" value="D"></input>
+			</div>
+			<div>
+				<h2>Question 4</h2>
+				<input type="radio" name="4" value="A"></input>
+				<input type="radio" name="4" value="B"></input>
+				<input type="radio" name="4" value="C"></input>
+				<input type="radio" name="4" value="D"></input>
+			</div>
+			<div>
+				<h2>Question 5</h2>
+				<input type="radio" name="5" value="A"></input>
+				<input type="radio" name="5" value="B"></input>
+				<input type="radio" name="5" value="C"></input>
+				<input type="radio" name="5" value="D"></input>
+			</div>
+			<div>
+				<h2>Question 6</h2>
+				<input type="radio" name="6" value="A"></input>
+				<input type="radio" name="6" value="B"></input>
+				<input type="radio" name="6" value="C"></input>
+				<input type="radio" name="6" value="D"></input>
+			</div>
+			<div>
+				<h2>Question 7</h2>
+				<input type="radio" name="7" value="A"></input>
+				<input type="radio" name="7" value="B"></input>
+				<input type="radio" name="7" value="C"></input>
+				<input type="radio" name="7" value="D"></input>
+			</div>
+			<div>
+				<h2>Question 8</h2>
+				<input type="radio" name="8" value="A"></input>
+				<input type="radio" name="8" value="B"></input>
+				<input type="radio" name="8" value="C"></input>
+				<input type="radio" name="8" value="D"></input>
+			</div>
+			<div>
+				<h2>Question 9</h2>
+				<input type="radio" name="9" value="A"></input>
+				<input type="radio" name="9" value="B"></input>
+				<input type="radio" name="9" value="C"></input>
+				<input type="radio" name="9" value="D"></input>
+			</div>
+			<div>
+				<h2>Question 10</h2>
+				<input type="radio" name="10" value="A"></input>
+				<input type="radio" name="10" value="B"></input>
+				<input type="radio" name="10" value="C"></input>
+				<input type="radio" name="10" value="D"></input>
+			</div>
+			<input type="submit" value="Submit"></input>
+		</form>
+	</body>
+	<script>
+		var answers = [];
+		var answerKey = [A,B,C,D,A,B,C,D,A,B];
+		var results = {};
+		function verify(){
+			for (var i = 0, l=this.length; i < l; i++)
+			{				
+				if (answers[i] != answerKey[i]) 
+				{ 				
+					results[i] = "Incorrect, please retry this question";
+				}
+				else
+				{
+					results[i] = "Correct!";
+				}
+			}       
+		}
+		function checkAnswers(){
+			for i = 0; i < 10; i ++)
+			{
+				answers[i] = document.getElementById(i).value;
+			}
+			if(answers.length != answerKey.length)
+			{
+				alert("you did not answer all of the questions");
+				return;
+			}
+			verify();
+			alert(results);
+		}
+	</script>
+</html>
