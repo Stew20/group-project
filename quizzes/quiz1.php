cb08a6a6983f16e32ca86ef8bbfd9bb8b627c7ad
Greg Griffith <cryptounitedteam@gmail.com>
2017-04-25 12:30:35 -0400

    added ids to search for answers on quiz
    added ids to search for answers on quiz

--- quizzes/quiz1.php
+++ quizzes/quiz1.php
@@ -11,73 +11,73 @@
 			<h1>Quiz1</h1>
 			<div>
 				<h2>Question 1</h2>
-				<input type="radio" name="1" value="A"></input>
-				<input type="radio" name="1" value="B"></input>
-				<input type="radio" name="1" value="C"></input>
-				<input type="radio" name="1" value="D"></input>
+				<input type="radio" name="1" id="1" value="A"></input>
+				<input type="radio" name="1" id="1" value="B"></input>
+				<input type="radio" name="1" id="1" value="C"></input>
+				<input type="radio" name="1" id="1" value="D"></input>
 			</div>
 			<div>
 				<h2>Question 2</h2>
-				<input type="radio" name="2" value="A"></input>
-				<input type="radio" name="2" value="B"></input>
-				<input type="radio" name="2" value="C"></input>
-				<input type="radio" name="2" value="D"></input>
+				<input type="radio" name="2" id="2" value="A"></input>
+				<input type="radio" name="2" id="2" value="B"></input>
+				<input type="radio" name="2" id="2" value="C"></input>
+				<input type="radio" name="2" id="2" value="D"></input>
 			</div>
 			<div>
 				<h2>Question 3</h2>
-				<input type="radio" name="3" value="A"></input>
-				<input type="radio" name="3" value="B"></input>
-				<input type="radio" name="3" value="C"></input>
-				<input type="radio" name="3" value="D"></input>
+				<input type="radio" name="3" id="3" value="A"></input>
+				<input type="radio" name="3" id="3" value="B"></input>
+				<input type="radio" name="3" id="3" value="C"></input>
+				<input type="radio" name="3" id="3" value="D"></input>
 			</div>
 			<div>
 				<h2>Question 4</h2>
-				<input type="radio" name="4" value="A"></input>
-				<input type="radio" name="4" value="B"></input>
-				<input type="radio" name="4" value="C"></input>
-				<input type="radio" name="4" value="D"></input>
+				<input type="radio" name="4" id="4" value="A"></input>
+				<input type="radio" name="4" id="4" value="B"></input>
+				<input type="radio" name="4" id="4" value="C"></input>
+				<input type="radio" name="4" id="4" value="D"></input>
 			</div>
 			<div>
 				<h2>Question 5</h2>
-				<input type="radio" name="5" value="A"></input>
-				<input type="radio" name="5" value="B"></input>
-				<input type="radio" name="5" value="C"></input>
-				<input type="radio" name="5" value="D"></input>
+				<input type="radio" name="5" id="5" value="A"></input>
+				<input type="radio" name="5" id="5" value="B"></input>
+				<input type="radio" name="5" id="5" value="C"></input>
+				<input type="radio" name="5" id="5" value="D"></input>
 			</div>
 			<div>
 				<h2>Question 6</h2>
-				<input type="radio" name="6" value="A"></input>
-				<input type="radio" name="6" value="B"></input>
-				<input type="radio" name="6" value="C"></input>
-				<input type="radio" name="6" value="D"></input>
+				<input type="radio" name="6" id="6" value="A"></input>
+				<input type="radio" name="6" id="6" value="B"></input>
+				<input type="radio" name="6" id="6" value="C"></input>
+				<input type="radio" name="6" id="6" value="D"></input>
 			</div>
 			<div>
 				<h2>Question 7</h2>
-				<input type="radio" name="7" value="A"></input>
-				<input type="radio" name="7" value="B"></input>
-				<input type="radio" name="7" value="C"></input>
-				<input type="radio" name="7" value="D"></input>
+				<input type="radio" name="7" id="7" value="A"></input>
+				<input type="radio" name="7" id="7" value="B"></input>
+				<input type="radio" name="7" id="7" value="C"></input>
+				<input type="radio" name="7" id="7" value="D"></input>
 			</div>
 			<div>
 				<h2>Question 8</h2>
-				<input type="radio" name="8" value="A"></input>
-				<input type="radio" name="8" value="B"></input>
-				<input type="radio" name="8" value="C"></input>
-				<input type="radio" name="8" value="D"></input>
+				<input type="radio" name="8" id="8" value="A"></input>
+				<input type="radio" name="8" id="8" value="B"></input>
+				<input type="radio" name="8" id="8" value="C"></input>
+				<input type="radio" name="8" id="8" value="D"></input>
 			</div>
 			<div>
 				<h2>Question 9</h2>
-				<input type="radio" name="9" value="A"></input>
-				<input type="radio" name="9" value="B"></input>
-				<input type="radio" name="9" value="C"></input>
-				<input type="radio" name="9" value="D"></input>
+				<input type="radio" name="9" id="9" value="A"></input>
+				<input type="radio" name="9" id="9" value="B"></input>
+				<input type="radio" name="9" id="9" value="C"></input>
+				<input type="radio" name="9" id="9" value="D"></input>
 			</div>
 			<div>
 				<h2>Question 10</h2>
-				<input type="radio" name="10" value="A"></input>
-				<input type="radio" name="10" value="B"></input>
-				<input type="radio" name="10" value="C"></input>
-				<input type="radio" name="10" value="D"></input>
+				<input type="radio" name="10" id="10" value="A"></input>
+				<input type="radio" name="10" id="10" value="B"></input>
+				<input type="radio" name="10" id="10" value="C"></input>
+				<input type="radio" name="10" id="10" value="D"></input>
 			</div>
 		</form>
 		<button onClick="checkAnswers()">Submit</button>
