c3b491694ddabb1aab31f07e65218c421c6d6131
Cenric <eric.cen@hotmail.com>
2017-04-25 13:05:40 -0400

    Added Tut4 Page

---
+++ tutorials/tutorial4.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php
+		$path = "./";
+		include'header.php';
+	?>
+	<head>
+	<title>Command</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">	
+	</head>
+	
+	<body>
+		<div class="tran">
+ 	   <img src="assets/images/code3.jpg" alt="picture" style="width:100vw;height:250px;">
+	</div>
+	
+	<?php
+		include'nav.php';
+	?>
+	
+
+	<h1>Unix - Package Manager </h1>
+	<div class="border1">
+		<div class="box1">
+			<h2>Listing File</h2>
+			<div class = "commBorder">
+				<p>ls</p>
+			</div>
+		<p>
+			(Describe what is 'ls' function)
+			xxx xxx xx xxx xx xxxx xx xxx xx xxx xx xxxx xx xxx xx xxx xx xxxx xx xxx xx xxx xx xxxx
+			xx xxx xx xxx xx xxxx xx xxx xx xxx xx xxxx xx xxx xx xxx xx xxxx xx xxx xx xxx xx xxxx 
+			<div class="textColor">
+				<p>
+				Following is Output Example:
+				<p>
+			</div>
+			
+			<img src="assets/images/ScreenShot.png" alt="picture" style="width:45vw;height:300px;">
+		</p>
+		
+		<hr>
+
+		<div class = "commBorder">
+		<p>ls -a</p>
+		</div>
+		
+		<p>
+		(Describe what is 'ls -a' function)
+		xxx xxx xx xxx xx xxxx xx xxx xx xxx xx xxxx xx xxx xx xxx xx xxxx xx xxx xx xxx xx xxxx 
+			<div class="textColor">
+				<p>
+				Following is Output Example:
+				<p>
+			</div>
+			<img src="assets/images/ScreenShot2.png" alt="picture" style="width:45vw;height:300px;">
+		</p>
+		
+		<hr>
+		
+		<h2>Summary</h2>
+		<table>
+		  <tr>
+			<th>Command</th>
+			<th>Function</th>
+		  </tr>
+		  <tr>
+			<td>ls</td>
+			<td>simple description function</td>
+		  </tr>
+		  <tr>
+			<td>ls -a</td>
+			<td>simple description function</td>
+		  </tr>
+		</table>
+		
+		
+		
+		</div>
+	</div>
+	
+	<?php
+		include'footer.php';
+	?>
+        
+	</body>
+</html>
