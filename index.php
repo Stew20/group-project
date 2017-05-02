0cc5d7b2099dbdff44e3ebb78a71700511d3c6dc
Greg Griffith <cryptounitedteam@gmail.com>
2017-05-02 00:17:03 -0400

    use percent not vw when not in body
    use percent not vw when not in body

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $_SESSION["sidenav"] = "home";
 	?>
 	<body>
 		<div class="tran">
- 	      <img src="assets/images/code3.jpg" alt="picture" style="width:100vw;height:400px;">
+ 	      <img src="assets/images/code3.jpg" alt="picture" style="width:100%; height:400px;">
 	    </div>
 	
 	<?php
