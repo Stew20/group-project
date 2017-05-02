e5085ebff69f0b2d65530c25b43df07cf2422e42
Cenric <eric.cen@hotmail.com>
2017-05-02 13:16:31 -0400

    Revert Changes, Attempt to Fix List in Resources

--- resources/index.php
+++ resources/index.php
@@ -19,59 +19,53 @@
 <ul id="resourceList">
 	<li>
 		<div class="border1">
-			<div class="box1">
 			<ul>
-				<li> <h3> Books </h3></li>
-					<li>
-						<ul>
-							<?php echo('<li> <img class="centerImage" src="'.$path.'assets/images/unixInANutshell.jpg" alt="Unix in a Nutshell"> <br></li>'); ?>
-							<li class="centerText"> Robbins, Arnold. UNIX in a Nutshell:. Beijing: O'Reilly, 2005. Print. </li>
-							<?php echo('<li> <img class="centerImage" src="'.$path.'assets/images/learningTheUnix.jpg" alt=" Learning the Unix Operating System"> <br> </li> ');?>
-							<li class="centerText"> Peek, Jerry D., Grace Todino, and John Strang. Learning the UNIX Operating System. Sebastopol, CA: O'Reilly, 2002. Print. </li>
-							<?php echo('<li> <img class="centerImage" src="'.$path.'assets/images/unixForDummies.jpg" alt="Unix for Dummies"> <br> </li> ');?>
-							<li class="centerText"> Levine, John R., and Margaret Levine. Young. Unix for Dummies. Hoboken, NJ: John Wiley & Sons, 2013. Print.  </li>
-						</ul>
-					</li>
-				</ul>
-			</div>
+			<li> <h3> Books </h3></li>
+				<li>
+					<ul>
+						<?php echo('<li> <img class="centerImage" src="'.$path.'assets/images/unixInANutshell.jpg" alt="Unix in a Nutshell"> <br></li>'); ?>
+						<li class="centerText"> Robbins, Arnold. UNIX in a Nutshell:. Beijing: O'Reilly, 2005. Print. </li>
+						<?php echo('<li> <img class="centerImage" src="'.$path.'assets/images/learningTheUnix.jpg" alt=" Learning the Unix Operating System"> <br> </li> ');?>
+						<li class="centerText"> Peek, Jerry D., Grace Todino, and John Strang. Learning the UNIX Operating System. Sebastopol, CA: O'Reilly, 2002. Print. </li>
+						<?php echo('<li> <img class="centerImage" src="'.$path.'assets/images/unixForDummies.jpg" alt="Unix for Dummies"> <br> </li> ');?>
+						<li class="centerText"> Levine, John R., and Margaret Levine. Young. Unix for Dummies. Hoboken, NJ: John Wiley & Sons, 2013. Print.  </li>
+					</ul>
+				</li>
+			</ul>
 		</div>
 	</li>
 	
 	<li>
 		<div class="border1">
-			<div class="box1">
-				<ul>
-					<li> <h3> PDFs </h3></li>
-					<li>
-						<ul>
-							<!-- <?php echo(' <li><a href="https://www.tutorialspoint.com/unix/unix_tutorial.pdf"> <img src="'.$path.'assets/images/firstRpdf.png" alt=" go to first PDF Recource" style="width=40px; height=50px;"> </a></li> ');?> -->
-							<li><a href="https://www.tutorialspoint.com/unix/unix_tutorial.pdf"> Unix Tutorial </a></li>
-							<!-- <?php echo(' <li><a href="http://downloads.tuxpuc.pucp.edu.pe/manuales/otros/UNIX.PDF"> <img src="'.$path.'assets/images/secondRpdf.png" alt=" go to second PDF Recource" style="width=40px; height=50px;"></a></li> ');?> -->
-							<li><a href="http://downloads.tuxpuc.pucp.edu.pe/manuales/otros/UNIX.PDF"> Teach Yourself Unix </a></li>
-							<li><a href="http://www.maths.manchester.ac.uk/~pjohnson/resources/unixShort/examples-commands.pdf"> Thirty Useful Unix Commands </a></li>
-							<li><a href="https://www.univie.ac.at/cartography/lehre/kvaf/links_und_tutorials/tutorials/unix.pdf"> Basic Unix Tutorial </a></li>
-						</ul>
-					</li>
-				</ul>
-			</div>
+			<ul>
+				<li> <h3> PDFs </h3></li>
+				<li>
+					<ul>
+						<!-- <?php echo(' <li><a href="https://www.tutorialspoint.com/unix/unix_tutorial.pdf"> <img src="'.$path.'assets/images/firstRpdf.png" alt=" go to first PDF Recource" style="width=40px; height=50px;"> </a></li> ');?> -->
+						<li><a href="https://www.tutorialspoint.com/unix/unix_tutorial.pdf"> Unix Tutorial </a></li>
+						<!-- <?php echo(' <li><a href="http://downloads.tuxpuc.pucp.edu.pe/manuales/otros/UNIX.PDF"> <img src="'.$path.'assets/images/secondRpdf.png" alt=" go to second PDF Recource" style="width=40px; height=50px;"></a></li> ');?> -->
+						<li><a href="http://downloads.tuxpuc.pucp.edu.pe/manuales/otros/UNIX.PDF"> Teach Yourself Unix </a></li>
+						<li><a href="http://www.maths.manchester.ac.uk/~pjohnson/resources/unixShort/examples-commands.pdf"> Thirty Useful Unix Commands </a></li>
+						<li><a href="https://www.univie.ac.at/cartography/lehre/kvaf/links_und_tutorials/tutorials/unix.pdf"> Basic Unix Tutorial </a></li>
+					</ul>
+				</li>
+			</ul>
 		</div>
 	</li>
 	
 	<li>
 		<div class="border1">
-			<div class="box1">
-				<ul>
-					<li> <h3> Websites </h3></li>
-					<li>
-						<ul>
-							<li class="leftText"><a href="https://www.unixtutorial.org/basic-unix-commands/">Basic Unix Commands</a></li>
-							<li class="leftText"><a href="https://www.unixtutorial.org/basic-unix-commands/">Advanced Unix Commands</a></li>
-							<li class="leftText"><a href="https://www.tutorialspoint.com/unix/index.htm"> Unix for Beginners</a></li>
-							<li class="leftText"><a href="https://www.tutorialspoint.com/unix/unix-regular-expressions.htm">Unix for Advanced</a></li>
-						</ul>
-					</li>	
-				</ul>
-			</div>
+			<ul>
+				<li> <h3> Websites </h3></li>
+				<li>
+					<ul>
+						<li class="leftText"><a href="https://www.unixtutorial.org/basic-unix-commands/">Basic Unix Commands</a></li>
+						<li class="leftText"><a href="https://www.unixtutorial.org/basic-unix-commands/">Advanced Unix Commands</a></li>
+						<li class="leftText"><a href="https://www.tutorialspoint.com/unix/index.htm"> Unix for Beginners</a></li>
+						<li class="leftText"><a href="https://www.tutorialspoint.com/unix/unix-regular-expressions.htm">Unix for Advanced</a></li>
+					</ul>
+				</li>	
+			</ul>
 		</div>
 	</li>
 	
