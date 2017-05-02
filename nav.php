d72851da96fa5b9fe204d123227bbf95222276a0
Greg Griffith <cryptounitedteam@gmail.com>
2017-05-02 00:48:09 -0400

    added closing quotes
    added closing quotes

--- nav.php
+++ nav.php
@@ -3,16 +3,16 @@
 	<!-- <li><a class="active" href="#home">Home</a></li> -->
 	<!-- <li><a class="home" href="#home"><img src = "HomeButton2.png" alt = "Home Button" style = "width: 35px; height: 35px;"> </li></a> -->
 	<?php
-		echo('<li><a class="nav_bar_button" sqlid="home" id="'.$path.'index.php" onclick="nav_button_clicked(this)>Home</a></li>');
+		echo('<li><a class="nav_bar_button" sqlid="home" id="'.$path.'index.php" onclick="nav_button_clicked(this)">Home</a></li>');
 	?>
 	<li><a href="#tutorials">Tutorials</a>
 		<ul class="tutorialsWidth">
 			<?php
-				echo('<li><a class="nav_bar_button" sqlid="tutorials" id="'.$path.'tutorials/tutorial1.php" onclick="nav_button_clicked(this)>1. OS Navigation</a></li>');
-				echo('<li><a class="nav_bar_button" sqlid="tutorials" id="'.$path.'tutorials/tutorial2.php" onclick="nav_button_clicked(this)>2. File Manipulation</a></li>');
-				echo('<li><a class="nav_bar_button" sqlid="tutorials" id="'.$path.'tutorials/tutorial3.php" onclick="nav_button_clicked(this)>3. File Permissions</a></li>');
-				echo('<li><a class="nav_bar_button" sqlid="tutorials" id="'.$path.'tutorials/tutorial4.php" onclick="nav_button_clicked(this)>4. Package Manager </a></li>');
-				echo('<li><a class="nav_bar_button" sqlid="tutorials" id="'.$path.'tutorials/index.php"     onclick="nav_button_clicked(this)>More Tutorials</a></li>');	
+				echo('<li><a class="nav_bar_button" sqlid="tutorials" id="'.$path.'tutorials/tutorial1.php" onclick="nav_button_clicked(this)">1. OS Navigation</a></li>');
+				echo('<li><a class="nav_bar_button" sqlid="tutorials" id="'.$path.'tutorials/tutorial2.php" onclick="nav_button_clicked(this)">2. File Manipulation</a></li>');
+				echo('<li><a class="nav_bar_button" sqlid="tutorials" id="'.$path.'tutorials/tutorial3.php" onclick="nav_button_clicked(this)">3. File Permissions</a></li>');
+				echo('<li><a class="nav_bar_button" sqlid="tutorials" id="'.$path.'tutorials/tutorial4.php" onclick="nav_button_clicked(this)">4. Package Manager </a></li>');
+				echo('<li><a class="nav_bar_button" sqlid="tutorials" id="'.$path.'tutorials/index.php"     onclick="nav_button_clicked(this)">More Tutorials</a></li>');	
 			?>
 		</ul>
 	</li>
@@ -28,9 +28,9 @@
 	<li><a href="#quizzes">Quizzes</a>
 		<ul class="quizzesWidth	">
 			<?php
-				echo('<li><a class="nav_bar_button" sqlid="quizzes" id="'.$path.'quizzes/index.php" onclick="nav_button_clicked(this)>Q1. Navigation</a></li>');
-				echo('<li><a class="nav_bar_button" sqlid="quizzes" id="'.$path.'quizzes/index.php" onclick="nav_button_clicked(this)>Q2. Files</a></li>');
-				echo('<li><a class="nav_bar_button" sqlid="quizzes" id="'.$path.'quizzes/index.php" onclick="nav_button_clicked(this)>Q3. Overview</a></li>');
+				echo('<li><a class="nav_bar_button" sqlid="quizzes" id="'.$path.'quizzes/index.php" onclick="nav_button_clicked(this)">Q1. Navigation</a></li>');
+				echo('<li><a class="nav_bar_button" sqlid="quizzes" id="'.$path.'quizzes/index.php" onclick="nav_button_clicked(this)">Q2. Files</a></li>');
+				echo('<li><a class="nav_bar_button" sqlid="quizzes" id="'.$path.'quizzes/index.php" onclick="nav_button_clicked(this)">Q3. Overview</a></li>');
 			?>
 		</ul>
 	</li>
@@ -39,10 +39,10 @@
 	?>
 		<ul class="resourcesWidth">
 			<?php
-				echo('<li><a class="nav_bar_button" sqlid="resources" id="'.$path.'resources/index.php#books"    onclick="nav_button_clicked(this)>Books</a></li> ');
-				echo('<li><a class="nav_bar_button" sqlid="resources" id="'.$path.'resources/index.php#pdfs"     onclick="nav_button_clicked(this)>PDFs</a></li>');	
-				echo('<li><a class="nav_bar_button" sqlid="resources" id="'.$path.'resources/index.php#websites" onclick="nav_button_clicked(this)>Links</a></li>');
-				echo('<li><a class="nav_bar_button" sqlid="resources" id="'.$path.'resources/index.php#footer"   onclick="nav_button_clicked(this)>Contact Us</a></li>');
+				echo('<li><a class="nav_bar_button" sqlid="resources" id="'.$path.'resources/index.php#books"    onclick="nav_button_clicked(this)">Books</a></li> ');
+				echo('<li><a class="nav_bar_button" sqlid="resources" id="'.$path.'resources/index.php#pdfs"     onclick="nav_button_clicked(this)">PDFs</a></li>');	
+				echo('<li><a class="nav_bar_button" sqlid="resources" id="'.$path.'resources/index.php#websites" onclick="nav_button_clicked(this)">Links</a></li>');
+				echo('<li><a class="nav_bar_button" sqlid="resources" id="'.$path.'resources/index.php#footer"   onclick="nav_button_clicked(this)">Contact Us</a></li>');
 			?>
 		</ul>		
 	</li>
