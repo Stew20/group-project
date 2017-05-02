897debe75f223364455e018008a2828e8bcc2a96
Greg Griffith <cryptounitedteam@gmail.com>
2017-05-01 23:51:58 -0400

    button no longer exists so we use this
    button no longer exists so we use this

--- assets/javascript/nav_bar_level1.js
+++ assets/javascript/nav_bar_level1.js
@@ -6,7 +6,7 @@ function nav_button_clicked(){
 			var hidden_element = document.createElement("input");
 			hidden_element.setAttribute("name", "pageTo");
 			hidden_element.setAttribute("type", "text");
-			hidden_element.setAttribute("value", button.id.toLowerCase());
+			hidden_element.setAttribute("value", this.id.toLowerCase());
 			pageForm.appendChild(hidden_element);
 			window.document.body.appendChild(pageForm);
 			pageForm.submit();
