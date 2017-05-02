d4a7494d096e70a33eab11a37c07035bba0f7343
Greg Griffith <cryptounitedteam@gmail.com>
2017-05-02 01:03:50 -0400

    fixed js2
    fixed js2

--- assets/javascript/nav_bar_level2.js
+++ assets/javascript/nav_bar_level2.js
@@ -1,10 +1,4 @@
-window.document.onload = function()
-{
-	var buttons = window.document.getElementsByClassName('nav_bar_button');
-	for(var i = 0; i < buttons.length; i++)
-	{
-		var button = buttons[i];
-		button.onclick = function(){
+function nav_button_clicked(obj){
 			var pageForm = window.document.createElement("form");
 			var path = "../process_nav_button.php";
 			pageForm.setAttribute("method", "post");
@@ -12,10 +6,15 @@ window.document.onload = function()
 			var hidden_element = document.createElement("input");
 			hidden_element.setAttribute("name", "pageTo");
 			hidden_element.setAttribute("type", "text");
-			hidden_element.setAttribute("value", button.innerHTML.toLowerCase());
+			hidden_element.setAttribute("value", obj.id.toLowerCase());
 			pageForm.appendChild(hidden_element);
+			
+			var hidden_element2 = document.createElement("input");
+			hidden_element2.setAttribute("name", "sqlEntry");
+			hidden_element2.setAttribute("type", "text");
+			hidden_element2.setAttribute("value", obj.getAttribute("sqlid").toLowerCase());
+			pageForm.appendChild(hidden_element2);
+			
 			window.document.body.appendChild(pageForm);
 			pageForm.submit();
 		}
-	}
-}
