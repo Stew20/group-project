a51df53c9d39def0cae2041047b56023b0edd748
Greg Griffith <cryptounitedteam@gmail.com>
2017-05-01 23:25:15 -0400

    adjusted javascript

---
+++ assets/javascript/nav_bar_level1.js
@@ -0,0 +1,22 @@
+window.document.onload = function()
+{
+	var buttons = window.document.getElementsByClassName('nav_bar_button');
+	for(var i = 0; i < buttons.length; i++)
+	{
+		var button = buttons[i];
+		button.onclick = function(){
+			console.log("onclick function added");
+			var pageForm = window.document.createElement("form");
+			var path = "./process_nav_button.php";
+			pageForm.setAttribute("method", "post");
+			pageForm.setAttribute("action" , path);
+			var hidden_element = document.createElement("input");
+			hidden_element.setAttribute("name", "pageTo");
+			hidden_element.setAttribute("type", "text");
+			hidden_element.setAttribute("value", button.id.toLowerCase());
+			pageForm.appendChild(hidden_element);
+			window.document.body.appendChild(pageForm);
+			pageForm.submit();
+		}
+	}
+}
--- assets/javascript/nav_bar_level2.js
+++ assets/javascript/nav_bar_level2.js
@@ -6,12 +6,12 @@ window.document.onload = function()
 		var button = buttons[i];
 		button.onclick = function(){
 			var pageForm = window.document.createElement("form");
-			var path = "./process_nav_button.php";
+			var path = "../process_nav_button.php";
 			pageForm.setAttribute("method", "post");
 			pageForm.setAttribute("action" , path);
 			var hidden_element = document.createElement("input");
 			hidden_element.setAttribute("name", "pageTo");
-			hidden_element.setAttribute("type", "hidden");
+			hidden_element.setAttribute("type", "text");
 			hidden_element.setAttribute("value", button.innerHTML.toLowerCase());
 			pageForm.appendChild(hidden_element);
 			window.document.body.appendChild(pageForm);
