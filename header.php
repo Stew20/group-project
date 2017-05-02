f4c2ac47f446a7472699862bc813b5beec51dea2
Greg Griffith <cryptounitedteam@gmail.com>
2017-05-01 23:38:36 -0400

    deleted sidenav dependancy
    deleted sidenav dependancy

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <head>
 	<meta charset=utf-8 />
 	<title>Page Title</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<?php
 		echo(' <link rel="stylesheet" type="text/css" href="' . $path . 'assets/css/styles.css"> ');
 	?>
