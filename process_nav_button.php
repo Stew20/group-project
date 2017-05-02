8a078dff1d3454e3b8e9a32b75441e04ceaca46b
Greg Griffith <cryptounitedteam@gmail.com>
2017-05-02 00:34:14 -0400

    remove dies
    remove dies. they are actually breaking working stuff

--- process_nav_button.php
+++ process_nav_button.php
@@ -10,19 +10,12 @@
 	$result = $conn->query($sql);
 	if($result->num_rows > 0)
 	{
-	// this loop should reselt all selected to 0 so that we can turn the proper one to =1
 		while($row=$result->FETCH_ASSOC()){
 			$sql2 = "update sections set selected=0 where id=". $row['id'] . "";
 			$result2 = $conn->query($sql2);
-			if($result2->affected_rows > 0){
-				$sql3 = "update sections set selected=1 where section_name='" . $sql_dest . "'";
-				$result3 = $conn->query($sql3);
-				header('Location: '.$destination);
-			}
-			else{
-				echo("result2 failure");
-				die();
-			}
+			$sql3 = "update sections set selected=1 where section_name='" . $sql_dest . "'";
+			$result3 = $conn->query($sql3);
+			header('Location: '.$destination);
 		}
 	}
 	else
