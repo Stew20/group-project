6fed578564af50051658f2c2dffe8f9abe2af3b7
yalingL <yxl4427@g.rit.edu>
2017-05-03 19:29:47 -0400

    add java script link

--- header.php
+++ header.php
@@ -3,5 +3,6 @@
 	<title>Page Title</title>
 	<?php
 		echo(' <link rel="stylesheet" type="text/css" href="' . $path . 'assets/css/styles.css"> ');
+		echo('<script type="text/javascript" src="'.$path.'assets/javascript/back_to_top.js"></script>');
 	?>
 </head>
