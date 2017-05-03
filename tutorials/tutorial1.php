716f863be12624dfeed828a70d1da524f7f06248
yalingL <yxl4427@g.rit.edu>
2017-05-03 19:10:11 -0400

    Add “back to top” button

--- tutorials/tutorial1.php
+++ tutorials/tutorial1.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
+	<script src="back_to_top.js"></script>
+	
 <?php
 	$path = "../";
 	include $path."header.php";
@@ -137,24 +139,7 @@
 	<?php
 		include $path."footer.php";
 	?>
-	<script>
-		// When the user scrolls down 20px from the top of the document, show the button
-		window.onscroll = function() {scrollFunction()};
-
-		function scrollFunction() {
-    			if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        			document.getElementById("myBtn").style.display = "block";
-   			 } else {
-       				 document.getElementById("myBtn").style.display = "none";
-    			}
-		}
-
-		// When the user clicks on the button, scroll to the top of the document
-		function topFunction() {
-    			document.body.scrollTop = 0;
-    			document.documentElement.scrollTop = 0;
-		}
-	</script>
+	
 	</body>
 </html>
 	
