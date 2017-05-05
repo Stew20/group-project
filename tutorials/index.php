2c76e913a32a2bb6cb698ba817c6f2a3e70da37f
Cenric <eric.cen@hotmail.com>
2017-05-05 11:23:34 -0400

    Tutorials Landing page fixes

--- tutorials/index.php
+++ tutorials/index.php
@@ -6,7 +6,17 @@
 	<?php
 		include'../header.php';
 	?>
-	<body>
+    
+<body>
+    <div class="tran">
+ 	      <?php echo('<img src="'.$path.'assets/images/code3.jpg" alt="picture" style="width:100vw;height:400px;">'); ?>
+	</div>
+	
+	<?php
+		include $path.'nav.php';
+	?>
+	<button onclick="topFunction()" id="myBtn" title="Go to top">⬆︎</button>
+    
 	<?php
       include "../php_connection.php";
       $sql = "select nav_index from sidenav_tutorials";
@@ -21,7 +31,8 @@
       else{
          echo "0 results, you did something bad";
       }
+    <?php
 		include'../footer.php';
 	?>
-	</body>
+</body>
 </html>
