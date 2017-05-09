bf77e90c666e3607995569b6c20ad7c2e0ee5a0e
yalingL <yxl4427@g.rit.edu>
2017-05-09 00:17:58 -0400

    Update header.php

--- header.php
+++ header.php
@@ -4,8 +4,5 @@
 	<?php
 		echo(' <link rel="stylesheet" type="text/css" href="' . $path . 'assets/css/styles.css"> ');
 		echo('<script type="text/javascript" src="'.$path.'assets/javascript/back_to_top.js"></script>');
-		echo('<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">');
-		echo('<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>');
-		echo('<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>');
 	?>
 </head>
