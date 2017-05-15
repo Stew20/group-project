5e4bdc30ee13a7c68ddeffc58d0c70a137b2ec2a
Cenric <eric.cen@hotmail.com>
2017-05-15 17:50:43 -0400

    Tutorial Numbering

--- tutorials/index.php
+++ tutorials/index.php
@@ -27,15 +27,15 @@
                 <h1> Other Tutorials </h1>
                 
                 <ul>
-                    <li> <a href="tutorial1.php"> OS Navigation </a> </li>
-                    <li> <a href="tutorial2.php"> File Manipulation </a></li>
-                    <li> <a href="tutorial3.php"> File Permissions </a></li>
-                    <li> <a href="tutorial4.php"> Package Manager </a></li>
-                    <li> <a href="tutorial5.php"> Users and User Variables</a></li>
-                    <li> <a href="tutorial6.php"> Using Sudo </a></li>
-                    <li> <a href="tutorial7.php"> The File System </a></li>
-                    <li> <a href="tutorial8.php"> Daemons </a></li>
-                    <li> <a href="tutorial9.php"> Manual Commands </a></li>
+                    <li> <a href="tutorial1.php"> 1. OS Navigation </a> </li>
+                    <li> <a href="tutorial2.php"> 2. File Manipulation </a></li>
+                    <li> <a href="tutorial3.php"> 3. File Permissions </a></li>
+                    <li> <a href="tutorial4.php"> 4. Package Manager </a></li>
+                    <li> <a href="tutorial5.php"> 5. Users and User Variables</a></li>
+                    <li> <a href="tutorial6.php"> 6. Using Sudo </a></li>
+                    <li> <a href="tutorial7.php"> 7. The File System </a></li>
+                    <li> <a href="tutorial8.php"> 8. Daemons </a></li>
+                    <li> <a href="tutorial9.php"> 9. Manual Commands </a></li>
                 </ul>            
             </div>
         </div>
