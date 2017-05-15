1e6facd158eaabdc294d24ae9f256f73985930c4
karnowsky1 <Student@howard04.istlabs.rit.edu>
2017-05-14 20:45:57 -0400

    Forgot the ketchup tho
    Forgot the ketchup tho

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ $_SESSION["sidenav"] = "home";
 		include $path.'header.php';
 	?>
 	<body>
-		<div class = "background">
-			<div class="imgContainer" >
-	 	      <img class="imgResp" src="assets/images/banner_words.png" alt="picture" >
+		<div class = "background" style ="height: 10em;">
+			<div style="text-align: center;" >
+	 	      <img class="imgResp" src="assets/images/banner_words.png" alt="picture" style="top:39px;">
 		   </div>
 		</div>
 
