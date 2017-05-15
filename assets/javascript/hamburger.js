50084078411f944bec57c7a8c88174c973b81508
karnowsky1 <Student@howard04.istlabs.rit.edu>
2017-05-14 20:05:30 -0400

    Where the buns at?
    Where the buns at?

--- assets/javascript/hamburger.js
+++ assets/javascript/hamburger.js
@@ -8,12 +8,12 @@
 
 function mobileNav(x) {
     x.classList.toggle("change");
-    if(document.getElementById("nav-inner").style.right == ""){
-        document.getElementById("nav-inner").style.right = "0px";
-    }else if(document.getElementById("nav-inner").style.right == "-130px"){
-        document.getElementById("nav-inner").style.right = "0px";
-    }else if(document.getElementById("nav-inner").style.right == "0px"){
-        document.getElementById("nav-inner").style.right = "-130px";
+    if(document.getElementById("nav").style.right == ""){
+        document.getElementById("nav").style.right = "0px";
+    }else if(document.getElementById("nav").style.right == "-130px"){
+        document.getElementById("nav").style.right = "0px";
+    }else if(document.getElementById("nav").style.right == "0px"){
+        document.getElementById("nav").style.right = "-130px";
     }
-    document.getElementById("nav-inner").style.transition = ".5s";
+    document.getElementById("nav").style.transition = ".5s";
 }
