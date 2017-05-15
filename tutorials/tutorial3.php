89e920bc4572df035fa38d48fe95527dce67bb15
yalingL <yxl4427@g.rit.edu>
2017-05-15 15:17:34 -0400

    file permission tutorial

--- tutorials/tutorial3.php
+++ tutorials/tutorial3.php
@@ -18,127 +18,178 @@
 		<button onclick="topFunction()" id="myBtn" title="Go to top">⬆︎</button>
 
 
-		<h1>Unix - File Permissions</h1>
-		<div class="border1">
-			<div class="box1">
-				<h2>Permissions</h2>
-				<div class = "commBorder">
-					<p>Chmod</p>
-				</div>
-				<p>
-					The ls command is used for listing the contents of a directory, and can be used to show the properties and permissions of files and folders inside the parent.
-				</p>
-
-				<p>
-					Typing “ls” in the terminal will print out all files and folders in the parent folder.
-				</p>
-
-				<div class="textColor">
-					<p>
-						Following is Output Example:
-					</p>
-				</div>
-
-				<div class="ridge">
-					<?php echo('<img id="Img" src="'.$path.'assets/images/ScreenShot.png" style="width:45vw;height:300px;">'); ?>
+			<h1>Unix - File Permissions</h1>
+	<div class="border1">
+	<div class="box1">
+	
+	<p>Permission are assigned to files in three categories:</p>
+	• Owner- The username to which the file belong.<br>
+	• Group - Who is a member of the group that a file belong to, can perform on the file.<br>
+	• Others - Everyone else on the system.
+	<p>Permissions for each of these categories:</p>
+	• r(read)- Username can read the contents.<br>
+	• w(write)- Username can add or delete contents.<br>
+	• x(execute)- Can execute the file.
+	
+	
+	<h2>Changing Permissions</h2>
+	<div class = "commBorder">
+	<p>Chmod</p>
+	</div>
+	<p>
+	To alter the file of the directory permissions, use the chmod command. Permission changes specified numerically or symbolically.
+	</p>
+	
+	<h2>Permissions Table</h2>
+	
+		<div id="commTable">
+	<table>
+  		<tr>
+    		<th>Symbolic</th>
+    		<th> Description</th>
+  		</tr>
+  
+  		<tr>
+    		<td>+</td>
+    		<td>Adds the permission to a file or directory.</td>
+  		</tr>
+  
+  		<tr>
+    		<td>-</td>
+    		<td>Delete the permission to a file or directory.</td>
+  		</tr>
+		
+		<tr>
+			<td>=</td>
+			<td>Sets the permission.</td>
+ 	 	</tr>
+ 
+	</table>
+		</div>	
+		 
+		 
+	<div id="commTable">
+	<table>
+  		<tr>
+    		<th>Number</th>
+    		<th>Description</th>
+    		<th>Ref</th>
+  		</tr>
+  
+  		<tr>
+    		<td>0</td>
+    		<td>No Permission</td>
+    		<td>--</td>
+  		</tr>
+  
+  		<tr>
+    		<td>1 </td>
+    		<td>Execute Permission</td>
+    		<td>--x</td>
+  		</tr>
+		
+		<tr>
+			<td>2</td>
+			<td>Write Permission</td>
+			<td>-w-</td>
+ 	 	</tr>
+ 	 	
+ 	 	<tr>
+			<td>3</td>
+			<td>Execute and write Permission</td>
+			<td>-wx</td>
+ 	 	</tr>
+ 		
+ 		<tr>
+			<td>4</td>
+			<td>Read Permission</td>
+			<td>r--</td>
+ 	 	</tr>
+ 	 	
+ 	 	<tr>
+			<td>5</td>
+			<td>Read and execute permission</td>
+			<td>r-x</td>
+ 	 	</tr>
+ 	 	
+ 	 	<tr>
+			<td>6</td>
+			<td>Read and write permission</td>
+			<td>rw-</td>
+ 	 	</tr>
+ 	 	
+ 	 	
+ 	 	 <tr>
+			<td>7</td>
+			<td>All permissions</td>
+			<td>rwx</td>
+ 	 	</tr>
+	</table>
+		 </div>	
+	
+	<div class="textColor">
+	<p>
+	Following is Output Example:
+	</p>
+	</div>
+	<div class="ridge">
+					<?php echo('<img id="Img" src="'.$path.'assets/images/ScreenShot6.png" style="width:45vw;height:300px;">'); ?>
 			    </div>
 
 				<div id="backgrd" class="backColor">
 				  	<span class="close">&times;</span>
 				  	<img class="backColor-image" id="img1">
 				  	<div id="caption"></div>
-				</div>
+	</div>
+	<hr>
+	
+	<h2>Group Owner</h2>
+	<div class = "commBorder">
+	<p>Chgrp</p>
+	</div>
+	<p>
+	To alter the group of a file, use the chgrp command.
+	</p>
+	<div class="textColor">
+	<p>
+	Following is Output Example:
+	</p>
+	</div>
+	<div class="ridge">
+		<?php echo('<img id="Img2" src="'.$path.'assets/images/ScreenShot7.png" alt="picture" style="width:45vw;height:300px;">'); ?>
+	</div>
 
+	<div id="backgrd2" class="backColor">
+	<span class="close">&times;</span>
+	<img class="backColor-image" id="img22">
+	<div id="caption2"></div>
+	</div>
+	<hr>
+	
+	
+	<h2>Change User Ownership</h2>
+	<div class = "commBorder">
 
-				<p>
-					For a more detailed description of folder contents, flags such as -l can be used. -l gives a “Long” listing of details, including permissions, ownership, creator, modification date, and name.
-				</p>
-				<div class="textColor">
-					<p>
-						Following is Output Example:
-					</p>
-				</div>
+	<p>Chown</p>
+	</div>
+	<p>
+	The chown command changes the ownership of file.
+	</p>
+	<div class="textColor">
+	<p>
+	Following is Output Example:
+	</p>
+	</div>
+	<div class="ridge">
+		<?php echo('<img id="Img3" src="'.$path.'assets/images/ScreenShot8.png" alt="picture" style="width:45vw;height:300px;">'); ?>
+	</div>
 
-				<div class="ridge">
-					<?php echo('<img id="Img2" src="'.$path.'assets/images/ScreenShot2.png" alt="picture" style="width:45vw;height:300px;">'); ?>
-				</div>
-
-				<div id="backgrd2" class="backColor">
-				<span class="close">&times;</span>
-				<img class="backColor-image" id="img22">
-				<div id="caption2"></div>
-				</div>
-
-				<hr>
-
-				<h2>Group Owner</h2>
-				<div class = "commBorder">
-					<p>Chgrp</p>
-				</div>
-				<p>
-					cd is used to change the current working directory. A working directory is where the user
-					is currently working from the command line.
-				</p>
-				<div class="textColor">
-					<p>
-						Following is Output Example:
-					</p>
-				</div>
-				<div class="ridge">
-					<?php echo('<img id="Img3" src="'.$path.'assets/images/ScreenShot3.png" alt="picture" style="width:45vw;height:300px;">'); ?>
-				</div>
-
-				<div id="backgrd3" class="backColor">
-				<span class="close">&times;</span>
-				<img class="backColor-image" id="img33">
-				<div id="caption3"></div>
-				</div>
-
-				<p>
-					cd followed by two periods ( .. ) is used to navigate up one folder to the parent folder.
-				</p>
-				<div class="textColor">
-					<p>
-						Following is Output Example:
-					</p>
-				</div>
-				<div class="ridge">
-					<?php echo('<img id="Img4" src="'.$path.'assets/images/ScreenShot4.png" alt="picture" style="width:45vw;height:300px;">'); ?>
-				</div>
-
-				<div id="backgrd4" class="backColor">
-				<span class="close">&times;</span>
-				<img class="backColor-image" id="img44">
-				<div id="caption4"></div>
-				</div>	
-				<hr>
-
-
-				<h2>change user ownership</h2>
-				<div class = "commBorder">
-					<p>Chown</p>
-				</div>
-				<p>
-					pwd does exactly what it says. It will print out the current working directory of the user to the terminal. This is helpful when navigating through a deep directory structure with many similarly named folders.
-				</p>
-				<p>
-					pwd is one of the few main Unix commands that is used by itself, without any options.
-				</p>
-				<div class="textColor">
-					<p>
-						Following is Output Example:
-					</p>
-				</div>
-
-				<div class="ridge">
-					<?php echo('<img id="Img5" src="'.$path.'assets/images/ScreenShot5.png" alt="picture" style="width:45vw;height:300px;">'); ?>
-				</div>
-		 		<div id="backgrd5" class="backColor">
-  				<span class="close">&times;</span>
-  				<img class="backColor-image" id="img55">
-  				<div id="caption5"></div>
-				</div>
-				
+	<div id="backgrd3" class="backColor">
+	<span class="close">&times;</span>
+	<img class="backColor-image" id="img33">
+	<div id="caption3"></div>
+	</div>
+					
 				<script>
 					// Get the background modal
 					var modal = document.getElementById('backgrd');
@@ -228,34 +279,34 @@
 
 			</script>
 				<hr>
+	<h2>Summary</h2>
+	<div id="commTable">
+	<table>
+  		<tr>
+    		<th>Command</th>
+    		<th>Function</th>
+  		</tr>
+  
+  		<tr>
+    		<td>chmod</td>
+    		<td>to change the permissions of a specified file or directory</td>
+  		</tr>
+  
+  		<tr>
+    		<td>chgrp</td>
+    		<td>changes the group of the file or directory specified by the File or Directory </td>
+  		</tr>
+		
+		<tr>
+			<td>chown</td>
+			<td>Change the owner and group of files</td>
+ 	 	</tr>
+ 
+		</table>
+		 </div>	
 
-				<h2>Summary</h2>
-				<div id="commTable">
-					<table>
-				  		<tr>
-				    		<th>Command</th>
-				    		<th>Function</th>
-				  		</tr>
 
-				  		<tr>
-				    		<td>ls</td>
-				    		<td>List Directories and Files</td>
-				  		</tr>
-
-				  		<tr>
-				    		<td>cd </td>
-				    		<td>change to the Directory</td>
-				  		</tr>
-
-						<tr>
-							<td>pwd</td>
-							<td>Display the current working Directory</td>
-				 	 	</tr>
-					</table>
-
-				</div>
-
-			</div>
+		</div>
 
 		</div>
 		
