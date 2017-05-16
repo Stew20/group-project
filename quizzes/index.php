ab518fcfe07a23883eedeb7a9cf299230269e6ad
karnowsky1 <Student@howard04.istlabs.rit.edu>
2017-05-16 08:23:24 -0400

    fixed quizzes landing page
    fixed quizzes landing page

--- quizzes/index.php
+++ quizzes/index.php
@@ -1,31 +1,30 @@
+<?php
+session_start();
+$_SESSION["sidenav"] = "home";
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
-    <title> Stew20 - Quizzes </title>
-</head>
 	<?php
-        $path = "../";
-		include'../header.php';
+		$path = "../";
+		include $path.'header.php';
 	?>
 	<body>
-        
-    <div class = "background">
-       <div class="imgContainer" >
-          <img class="imgResp" src="../assets/images/banner_words.png" alt="picture" >
-       </div>
-    </div>
-    
-    <?php
-		include $path.'nav.php';
+		<div class = "background" style ="height: 10em;">
+			<div style="text-align: center;" >
+	 	      <img class="imgResp" src="assets/images/banner_words.png" alt="picture" style="top:39px;">
+		   </div>
+		</div>
+
+	<?php
+		include 'nav.php';
 	?>
-    
+
     <button onclick="topFunction()" id="myBtn" title="Go to top">⬆︎</button>
-        
+
     <div id="page">
         <div class="border1">
             <div class="box1">
-                <h1> Quizzes </h1>                
+                <h1> Quizzes </h1>
                 <ul>
                     <li> <a href="quiz1.php"> OS Navigation </a> </li>
                     <li> <a href="quiz2.php"> File Manipulation </a></li>
@@ -34,11 +33,11 @@
  					<li> <a href="quiz5.php"> Using Sudo </a></li>
  					<li> <a href="quiz6.php"> Services </a></li>
  					<li> <a href="quiz7.php"> Setting up a webserver </a></li>
-                </ul>            
+                </ul>
             </div>
         </div>
     </div>
-    
+
     <?php
 		include'../footer.php';
 	?>
