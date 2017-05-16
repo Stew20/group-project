b444c0a8f12250175c75782199b90666f645ca8e
Greg Griffith <cryptounitedteam@gmail.com>
2017-05-16 06:00:32 -0400

    quiz2 complete

--- quizzes/quiz2.php
+++ quizzes/quiz2.php
@@ -15,54 +15,51 @@
 		include $path.'nav.php';
 	?>
 			<div class="border1">
-			<h1>Quiz1: OS Navigation</h1>
+			<h1>Quiz2: File Manipulation</h1>
 			<div>
-				<h5>Question 1: What command is used to list a directories contents?</h5>
+				<h5>Question 1: What command is used to make a new directory?</h5>
 				<div class="textColor">
-				<input type="radio" name="1" class="1" value="A">ls</input><br/>
-				<input type="radio" name="1" class="1" value="B">cd</input><br/>
-				<input type="radio" name="1" class="1" value="C">pwd</input><br/>
-				<input type="radio" name="1" class="1" value="D">none of the above</input>
+				<input type="radio" name="1" class="1" value="A">rmdir</input><br/>
+				<input type="radio" name="1" class="1" value="B">rm</input><br/>
+				<input type="radio" name="1" class="1" value="C">mkdir</input><br/>
+				<input type="radio" name="1" class="1" value="D">touch</input>
 				</div>
 
 			</div>
 			<div>
-				<h5>Question 2: What command is used to change directories?</h5>
+				<h5>Question 2: What command is the difference between touch foo and vim foo?</h5>
 				<div class="textColor">
-				<input type="radio" name="2" class="2" value="A">ls</input><br/>
-				<input type="radio" name="2" class="2" value="B">cd</input><br/>
-				<input type="radio" name="2" class="2" value="C">pwd</input><br/>
-				<input type="radio" name="2" class="2" value="D">none of the above</input>
+				<input type="radio" name="2" class="2" value="A">There isnt a difference</input><br/>
+				<input type="radio" name="2" class="2" value="B">touch will create the file, vim will create and open the file</input><br/>
+				<input type="radio" name="2" class="2" value="C">touch will create and open the file, vim will only create the file</input><br/>
+				<input type="radio" name="2" class="2" value="D">touch will delete the file, vim will create the file</input>
 				</div>
 
 			</div>
 			<div>
 
-				<h5>Question 3: What command displays the current location in the filesystem?</h5>
+				<h5>Question 3: It is possible to delete a directory with rm</h5>
 				<div class="textColor">
-				<input type="radio" name="3" class="3" value="A">ls</input><br/>
-				<input type="radio" name="3" class="3" value="B">cd</input><br/>
-				<input type="radio" name="3" class="3" value="C">pwd</input><br/>
-				<input type="radio" name="3" class="3" value="D">none of the above</input>
+				<input type="radio" name="3" class="3" value="A">True</input><br/>
+				<input type="radio" name="3" class="3" value="B">False</input><br/>
 				</div>
 			</div>
 			<div>
-				<h5>Question 4: Select none of the above</h5>
+				<h5>Question 4: What is the difference between rm and rmdir</h5>
 				<div class="textColor">
-				<input type="radio" name="4" class="4" value="A">ls</input><br/>
-				<input type="radio" name="4" class="4" value="B">cd</input><br/>
-				<input type="radio" name="4" class="4" value="C">pwd</input><br/>
-				<input type="radio" name="4" class="4" value="D">none of the above</input>
+				<input type="radio" name="4" class="4" value="A">rm removes files and directories, rmdir only removes directories</input><br/>
+				<input type="radio" name="4" class="4" value="B">rm only removes directories, rmdir removes files and directories</input><br/>
+				<input type="radio" name="4" class="4" value="C">they are the same command</input><br/>
 				</div>
 
 			</div>
 			<div>
-				<h5>Question 5: How would change directory to my parent directory</h5>
+				<h5>Question 5: What command moves a file, foo, to its parent directory</h5>
 				<div class="textColor">
-				<input type="radio" name="5" class="5" value="A">cd ..</input><br/>
-				<input type="radio" name="5" class="5" value="B">cd ~ </input><br/>
-				<input type="radio" name="5" class="5" value="C">the full file path is needed</input><br/>
-				<input type="radio" name="5" class="5" value="D">cant be done</input>
+				<input type="radio" name="5" class="5" value="A">mv foo ../</input><br/>
+				<input type="radio" name="5" class="5" value="B">mv foo ./</input><br/>
+				<input type="radio" name="5" class="5" value="C">mv foo ~</input><br/>
+				<input type="radio" name="5" class="5" value="D">mv foo parent</input>
 				</div>
 
 			</div>
@@ -75,7 +72,7 @@
 	</body>
 	<script>
 		var answers = [];
-		var answerKey = ["A","B","C","D","A"];
+		var answerKey = ["C","B","A","A","A"];
 		var results = [];
 		function verify(){
 			for (var i = 0; i < answers.length; i++)
