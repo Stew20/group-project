cc9ded12e7eab54116adc7f4f51faa061dbe5f42
Greg Griffith <cryptounitedteam@gmail.com>
2017-05-16 01:44:01 -0400

    update tutorials index

--- tutorials/index.php
+++ tutorials/index.php
@@ -24,7 +24,7 @@
     <div id="page">
         <div class="border1">
             <div class="box1">
-                <h1> Other Tutorials </h1>
+                <h1> Tutorials </h1>
                 
                 <ul>
                     <li> <a href="tutorial1.php"> 1. OS Navigation </a> </li>
@@ -33,9 +33,7 @@
                     <li> <a href="tutorial4.php"> 4. Package Manager </a></li>
                     <li> <a href="tutorial5.php"> 5. Users and User Variables</a></li>
                     <li> <a href="tutorial6.php"> 6. Using Sudo </a></li>
-                    <li> <a href="tutorial7.php"> 7. The File System </a></li>
                     <li> <a href="tutorial8.php"> 8. Daemons </a></li>
-                    <li> <a href="tutorial9.php"> 9. Manual Commands </a></li>
                 </ul>            
             </div>
         </div>
